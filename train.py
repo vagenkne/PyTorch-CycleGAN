8f23fbc816a012b0e5a13d86d05978fd48b27838
vagenkne <vagenkne@ktchpccg017.cm.cluster>
2019-06-10 12:04:19 -0400

    1:

--- train.py
+++ train.py
@@ -27,18 +27,20 @@ from datasets import ImageDataset
 
 #torch.Tensor.describe = _describe
 
+torch.cuda.set_device(2)
+
 parser = argparse.ArgumentParser()
 parser.add_argument('--epoch', type=int, default=0, help='starting epoch')
 parser.add_argument('--n_epochs', type=int, default=200, help='number of epochs of training')
 parser.add_argument('--batchSize', type=int, default=1, help='size of the batches')
-parser.add_argument('--dataroot', type=str, default='datasets/apple2orange/', help='root directory of the dataset')
+parser.add_argument('--dataroot', type=str, default='../pytorch-CycleGAN-and-pix2pix/datasets/synth', help='root directory of the dataset')
 parser.add_argument('--lr', type=float, default=0.0002, help='initial learning rate')
 parser.add_argument('--decay_epoch', type=int, default=100, help='epoch to start linearly decaying the learning rate to 0')
 parser.add_argument('--size', type=int, default=256, help='size of the data crop (squared assumed)')
 parser.add_argument('--input_nc', type=int, default=3, help='number of channels of input data')
 parser.add_argument('--output_nc', type=int, default=3, help='number of channels of output data')
 parser.add_argument('--cuda', action='store_true', help='use GPU computation')
-parser.add_argument('--n_cpu', type=int, default=0, help='number of cpu threads to use during batch generation')
+parser.add_argument('--n_cpu', type=int, default=1, help='number of cpu threads to use during batch generation')
 opt = parser.parse_args()
 print(opt)
 
